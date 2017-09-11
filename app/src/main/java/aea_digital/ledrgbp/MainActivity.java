ea9ec57d367d20e354957e111756f3cc91d53498
AlessioVilla92 <clalessio@gmail.com>
2017-09-11 23:14:16 +0200

    CommitFinal

---
+++ app/src/main/java/aea_digital/ledrgbp/MainActivity.java
@@ -0,0 +1,16 @@
+package aea_digital.ledrgbp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Per il primo commit
+        //2222
+        //aedsawsdfada
+    }
+}
